00faafc102fdf44dc26896fe878e3b6beab433af
angel navea <angelnaveanavea@gmail.com>
2025-05-06 09:26:48 -0400

    ordenacion

---
+++ js/intruciones js/script.js
@@ -0,0 +1,60 @@
+//prueba de conexion js
+console.log("conexion establecida con js");
+
+//numeroEntero
+
+let num =25;
+
+function operacion(){
+    return num;
+}
+console.log(operacion());
+
+//numeroDecimal
+
+let number=3.14;
+
+function mostrarDecimal(){
+    return number;
+}
+console.log(mostrarDecimal());
+
+//cadenaTexto
+
+let texto="Hola mundo";
+
+function returntexto(){
+    return texto;
+}
+console.log(returntexto());
+
+//valorBooleano
+
+let boolean= true;
+
+function returnbolean(){
+    return boolean;
+}
+console.log(returnbolean());
+
+//arregloNumeros
+let Arreglo= [1,2,3,4];
+
+function returnArreglo(){
+    return Arreglo;
+}
+console.log(returnArreglo());
+
+//objetoPersona
+
+let Persona= {
+    nombre: "angel",
+    edad: 16
+};
+
+function returnPersona(){
+    return Persona;
+}
+console.log(returnPersona());
+
+
