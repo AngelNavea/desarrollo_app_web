bc774b52e70820aeec9dcbffe5d2cc12f9d56413
angel navea <angelnaveanavea@gmail.com>
2025-05-07 12:54:56 -0400

    tarea2.1

--- js/intruciones js/script.js
+++ js/intruciones js/script.js
@@ -50,11 +50,65 @@ console.log(returnArreglo());
 let Persona= {
     nombre: "angel",
     edad: 16
-};
+};  
 
 function returnPersona(){
     return Persona;
 }
 console.log(returnPersona());
 
+//valorIndefinido
 
+let infi;
+
+function returninfi(){
+    return infi;
+}
+
+console.log(returninfi())
+
+//valorNulo
+
+let nulo =null;
+
+function returnnulo(){
+    return nulo;
+}
+console.log(returnnulo())
+
+//identificadorUnico
+
+let identidad= Symbol("id")
+
+function returnidentidad(){
+    return identidad;
+}
+console.log(returnidentidad())
+
+//numeroGrande
+
+let numgrande = BigInt(222222222)
+
+function returnnumgreande(){
+    return numgrande;
+}
+
+console.log(returnnumgreande())
+
+//mes contante
+
+const mes="mayo";
+
+function returnmes(){
+    return mes;
+}
+
+console.log(returnmes())
+
+//operacion
+
+function operacion(){
+    return 1 + 2
+}
+
+console.log(operacion())
