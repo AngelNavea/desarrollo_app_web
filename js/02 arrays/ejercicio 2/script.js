9ec7cf637465549ea76e81a45a041e085fcabf82
angel navea <angelnaveanavea@gmail.com>
2025-05-15 09:29:40 -0400

    arrays

---
+++ js/02 arrays/ejercicio 2/script.js
@@ -0,0 +1,6 @@
+function sumaExtremos() {
+    let numeros=[2,7,5,10,3]
+    let suma = numeros[0] + numeros[numeros.length-1];
+    alert("la suma de el primer y el ultimo numero es: \n" + suma);
+    
+}
