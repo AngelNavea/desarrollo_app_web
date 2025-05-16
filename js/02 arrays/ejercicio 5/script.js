a9b4ad80b08ebc5271582a80db026f0d568cc106
angel navea <angelnaveanavea@gmail.com>
2025-05-16 12:26:14 -0400

    otro arreglo

--- js/02 arrays/ejercicio 5/script.js
+++ js/02 arrays/ejercicio 5/script.js
@@ -1,6 +1,6 @@
 function mensajeConcatenado() {
-    let numeros_y_letras =["bazinga",7,4,2,6,2,8]
-    let palabra = numeros_y_letras[0];
-    let suma = numeros_y_letras[2]+numeros_y_letras[4];
+    let numeros =[7,4,2,6,2,8]
+    let palabra = "bazinga"
+    let suma = numeros[2]+numeros[4];
     alert("el resultado de sumar el segundo numero con el cuarto numero nos da: \n" + suma + palabra);
 }
