eaa9d1cbb54590741f0c84ffc66736796f5b20b8
angel navea <angelnaveanavea@gmail.com>
2025-05-16 12:21:53 -0400

    pequeño arreglo

--- js/02 arrays/ejercicio 4/script.js
+++ js/02 arrays/ejercicio 4/script.js
@@ -1,5 +1,5 @@
 function multiplicaPares() {
     let numeros =[5,7,4,2,6,2,8]
-    let multiplicacion = numeros[0]*numeros[2]*numeros[4]
+    let multiplicacion = numeros[0]*numeros[2]*numeros[4];
     alert("el resultado de muktiplicar los numeros pares es de: \n" + multiplicacion);
 }
